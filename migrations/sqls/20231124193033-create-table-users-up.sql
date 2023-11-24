d7e412ff729526d4a1a0e89b5d624c6ff46f6905
Kucing Basah 737 <kucingbasah737@gmail.com>
2023-11-25 03:06:37 +0700

    field super pada users

--- migrations/sqls/20231124193033-create-table-users-up.sql
+++ migrations/sqls/20231124193033-create-table-users-up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `redirector`.`users` (
-  `email` VARCHAR(256) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL , 
-  `password` VARCHAR(69) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL , 
-  `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
-  `disabled` TINYINT(1) NOT NULL DEFAULT '0' , 
+  `email` VARCHAR(256) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+  `password` VARCHAR(69) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
+  `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `disabled` TINYINT(1) NOT NULL DEFAULT '0' ,
+  `super` TINYINT(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`email`)
 ) ENGINE = InnoDB;
