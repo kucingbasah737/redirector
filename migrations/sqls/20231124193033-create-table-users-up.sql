733593faab3b7b863e5954f4be0f0fe353468974
Kucing Basah 737 <kucingbasah737@gmail.com>
2023-11-25 03:11:52 +0700

    Buang nama db pada create table users

--- migrations/sqls/20231124193033-create-table-users-up.sql
+++ migrations/sqls/20231124193033-create-table-users-up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `redirector`.`users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `email` VARCHAR(256) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
   `password` VARCHAR(69) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
   `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
