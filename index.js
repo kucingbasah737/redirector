aae52ecd2d98ddcbec6544faf7472482afda1d4d
Kucing Basah 737 <kucingbasah737@gmail.com>
2023-11-25 02:04:19 +0700

    include pid on log

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const MODULE_NAME = 'MAIN';
+
+const logger = require('./lib/logger');
+const sdNotify = require('./lib/sd-notify');
+
+logger.info(`${MODULE_NAME} 300582D4: Starting`);
+
 (async () => {
+  if (process.env.SYSTEMD_NOTIFY) {
+    sdNotify();
+  }
 })();
