89d2eacc81618fda3526190a6c7561ad012b631b
Kucing Basah 737 <kucingbasah737@gmail.com>
2023-11-25 03:01:50 +0700

    webserver

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const MODULE_NAME = 'MAIN';
 
+process.chdir(__dirname);
+
+require('dotenv').config();
+
 const logger = require('./lib/logger');
 const sdNotify = require('./lib/sd-notify');
 
 logger.info(`${MODULE_NAME} 300582D4: Starting`);
+
+require('./lib/web');
 
 (async () => {
   if (process.env.SYSTEMD_NOTIFY) {
