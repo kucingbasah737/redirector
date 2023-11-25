f2bbc113ab31e8edcafea6ab2101350f203a5216
Kucing Basah 737 <kucingbasah737@gmail.com>
2023-11-25 18:10:08 +0700

    Create script to update geoip database

---
+++ update-geoip-database.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+
+source .env
+
+STARDIR=`pwd`
+
+if [ -z "MAXMIND_LICENSE_KEY" ]; then
+  echo Please set MAXMIND_LICENSE_KEY environtment variable
+  exit 1
+fi
+
+cd node_modules/geoip-lite && npm run-script updatedb license_key=$MAXMIND_LICENSE_KEY
+
+cd $STARDIR
