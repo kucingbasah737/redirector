5043728d54d5bd4ec54fe47afc5104f282471e5e
Kucing Basah 737 <kucingbasah737@gmail.com>
2023-11-27 02:06:44 +0700

    Move update-password cmd to separate file

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ process.chdir(__dirname);
 const fs = require('node:fs/promises');
 
 require('dotenv').config();
-const prompt = require('prompt');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 // const logger = require('./lib/logger');
 const sdNotify = require('./lib/sd-notify');
 
+const updatePassword = require('./lib/cmd/update-password');
 const insertTarget = require('./lib/cmd/insert-target');
 const insertHostname = require('./lib/cmd/insert-hostname');
 const hostnameList = require('./lib/cmd/hostname-list');
@@ -35,48 +35,9 @@ const runWebServer = async () => {
   await fs.writeFile('pid.txt', process.pid.toString());
 };
 
-const updatePasswordCmd = async () => {
-  // eslint-disable-next-line global-require
-  const updatePassword = require('./lib/update-password');
-
-  prompt.start();
-  const promptResult = await prompt.get([
-    {
-      name: 'email',
-      message: 'User email',
-      required: true,
-    },
-    {
-      name: 'newPassword',
-      message: 'New password',
-      required: true,
-      hidden: true,
-    },
-    {
-      name: 'newPassword1',
-      message: 'Retype new password',
-      required: true,
-      hidden: true,
-    },
-  ]);
-
-  if (promptResult.newPassword !== promptResult.newPassword1) {
-    console.warn('Password does not match');
-    process.exit(0);
-  }
-
-  try {
-    await updatePassword(null, promptResult.email, promptResult.newPassword);
-    console.log(`Password for ${promptResult.email} updated`);
-  } catch (e) {
-    console.warn('Exception');
-  }
-  process.exit(0);
-};
-
 // eslint-disable-next-line no-unused-vars
 const { argv } = yargs(hideBin(process.argv))
-  .command('update-password', 'update user password', () => {}, updatePasswordCmd)
+  .command('update-password', 'update user password', () => {}, updatePassword)
   .command('insert-target', 'insert a new target', () => {}, insertTarget)
   .command('insert-hostname', 'insert a new hostname', () => {}, insertHostname)
   .command('hostname-list', 'hostname list', () => {}, hostnameList)
