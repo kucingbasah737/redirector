82c20292839de3d555726cc0b49cba505d0c7a9b
Kucing Basah 737 <kucingbasah737@gmail.com>
2023-11-30 18:43:05 +0700

    Create GEOTMPDIR and GEOTMPDIR

--- update-geoip-database.sh
+++ update-geoip-database.sh
@@ -3,11 +3,23 @@ set -x
 
 source .env
 
-STARDIR=`pwd`
+STARTDIR=`pwd`
 
 if [ -z "MAXMIND_LICENSE_KEY" ]; then
   echo Please set MAXMIND_LICENSE_KEY environtment variable
   exit 1
+fi
+
+if [ ! -z "$GEOTMPDIR" ]; then
+  if [ ! -d "$GEOTMPDIR" ]; then
+    mkdir -p $GEOTMPDIR
+  fi
+fi
+
+if [ ! -z "$GEODATADIR" ]; then
+  if [ ! -d "$GEODATADIR" ]; then
+    mkdir -p $GEODATADIR
+  fi
 fi
 
 export GEOTMPDIR
