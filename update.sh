85c28dd507f7f29d1da598eb0ca96d84d2dc6c0c
Kucing Basah 737 <kucingbasah737@gmail.com>
2023-11-30 17:41:59 +0700

    Progress on make an update script

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+
+if ! command -v jq &>/dev/null
+then
+  echo jq not found
+  exit 1
+fi
+
+OLDDEP="`jq -c .dependencies package.json`"
+
+echo "** Pulling from repo"
+git pull
+
+NEWDEP="`jq -c .dependencies package.json`"
+
+if [ "$OLDDEP" == "$NEWDEP" ]; then
+  echo "** Does not need to run npm ci"
+else
+  echo "** Need to run npm ci"
+  npm ci
+fi
